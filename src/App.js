17e1527cad6d83f285a0a3d34b7fc5c7469c796f
ayushranjan2806 <84095016+ayushranjan2806@users.noreply.github.com>
2024-05-09 11:53:53 +0530

    ranjan commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Home from './components/Home';
-import Jobs from './components/Jobs';
+// import Home from './components/Home';
+// import Jobs from './components/Jobs';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import PostJob from './components/PostJob';
-import SaveJobs from './components/SaveJobs';
-import Discussion from './components/Discussion';
-import ErrorPage from './components/ErrorPage';
-import ApplyJobs from './components/ApplyJobs';
+// import PostJob from './components/PostJob';
+// import SaveJobs from './components/SaveJobs';
+// import Discussion from './components/Discussion';
+import ErrorPage from './component/ErrorPage';
+// import ApplyJobs from './components/ApplyJobs';
 
 function App() {
   return (
@@ -14,14 +14,14 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/">
-            <Route index element={<Home />} />
+            {/* <Route index element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/jobs" element={<Jobs />} />
             <Route path="/post-job" element={<PostJob />} />
             <Route path="/apply-jobs" element={<ApplyJobs />} />
             <Route path="/saved-job" element={<SaveJobs />} />
-            <Route path="/discussion" element={<Discussion />} />
-            <Route path="*" element={<ErrorPage />} />
+            <Route path="/discussion" element={<Discussion />} /> */}
+            <Route path="/" element={<ErrorPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
